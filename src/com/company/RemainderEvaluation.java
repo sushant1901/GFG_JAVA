680f8ada8a52849aaf6aa4e80a975eeb62274d7d
Sushant Mishra <mishrasushant1901@gmail.com>
2022-01-18 21:41:10 +0530

    Remainder Evaluation

---
+++ src/com/company/RemainderEvaluation.java
@@ -0,0 +1,32 @@
+package com.company;
+import java.io.*;
+import java.util.*;
+
+class RemainderEvaluation
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int num1=sc.nextInt();
+            int num2=sc.nextInt();
+
+            Solution ob = new Solution();
+            int ans  = ob.findRemainder(num1,num2);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int findRemainder(int num1,int num2)
+    {
+        return num1%num2;
+    }
+}
