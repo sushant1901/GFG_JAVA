3c6e46688a304174d8fc095b33643e447a80dbab
Sushant Mishra <mishrasushant1901@gmail.com>
2022-02-16 10:15:43 +0530

    insertion in linked list

---
+++ src/com/company/GFG19.java
@@ -0,0 +1,89 @@
+package com.company;
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+class GFG19{
+    static void printList(Node node)
+    {
+        while (node != null)
+        {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t > 0){
+            int n = sc.nextInt();
+            Node head = null;
+            for(int i=0; i<n; i++)
+            {
+                int value = sc.nextInt();
+                int indicator = sc.nextInt();
+
+                Solution3 ob = new Solution3();
+                if(indicator == 0)
+                    head = ob.insertAtBeginning(head, value);
+                else
+                    head = ob.insertAtEnd(head, value);
+            }
+            printList(head);
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*
+class Node{
+    int data;
+    Node next;
+
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+*/
+class Solution3
+{
+    //Function to insert a node at the beginning of the linked list.
+    Node insertAtBeginning(Node head, int x)
+    {
+        // code here
+        Node newNode = new Node(x);
+        newNode.next = head;
+        head = newNode;
+        return head;
+    }
+
+    //Function to insert a node at the end of the linked list.
+    Node insertAtEnd(Node head, int x)
+    {
+        // code here
+        Node newNode = new Node(x);
+        if(head == null){
+            head = newNode;
+            return newNode;
+        }
+        Node curNode = head;
+        while(curNode.next != null){
+            curNode = curNode.next;
+        }
+        curNode.next = newNode;
+
+        return head;
+    }
+}
