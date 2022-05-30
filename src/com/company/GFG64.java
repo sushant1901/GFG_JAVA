a267b247cc23e17be15314879054392ad06fdc53
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2022-05-30 11:56:54 +0530

    Find MIN and MAX

---
+++ src/com/company/GFG64.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class pair  
+{  
+    long first, second;  
+    public pair(long first, long second)  
+    {  
+        this.first = first;  
+        this.second = second;  
+    }  
+}
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            
+            Compute obj = new Compute();
+            pair product = obj.getMinMax(a, n); 
+            System.out.println(product.first+" "+product.second);
+            
+        }
+	}
+}
+
+
+
+
+
+class Compute 
+{
+    static pair getMinMax(long a[], long n)  
+    {
+        //Write your code here
+        long min=a[0],max=a[0];
+        for(int i=0;i<a.length;i++){
+            if(min>a[i]) min=a[i];
+            if(max<a[i]) max=a[i];
+            
+        }
+        
+        pair ans= new pair(min,max);
+        return ans;
+    }
+}
