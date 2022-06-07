4e968e6f2db27af9d772912084b6dc82724c3da0
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2022-06-07 11:27:20 +0530

    Implement Stack using Linked List

---
+++ src/com/company/GFG68.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+class StackNode {
+    int data;
+    StackNode next;
+    StackNode(int a) {
+        data = a;
+        next = null;
+    }
+}
+
+class GfG {
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0) {
+            MyStack obj = new MyStack();
+            int Q = sc.nextInt();
+            while (Q > 0) {
+                int QueryType = 0;
+                QueryType = sc.nextInt();
+                if (QueryType == 1) {
+                    int a = sc.nextInt();
+                    obj.push(a);
+                } else if (QueryType == 2) {
+                    System.out.print(obj.pop() + " ");
+                }
+                Q--;
+            }
+            System.out.println("");
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class MyStack 
+{
+    // class StackNode {
+    //     int data;
+    //     StackNode next;
+    //     StackNode(int a) {
+    //         data = a;
+    //         next = null;
+    //     }
+    // }   
+    StackNode head=null;
+    
+    //Function to push an integer into the stack.
+    void push(int a) 
+    {
+        StackNode newNode = new StackNode(a);
+        if(head==null){
+            head=newNode;
+            return;
+        }
+        newNode.next=head;
+        head=newNode;
+        // Add your code here
+    }
+    
+    //Function to remove an item from top of the stack.
+    int pop() 
+    {
+        if(head==null) return -1;
+        StackNode top=head;
+        head=head.next;
+        return top.data;
+        // Add your code here
+    }
+}
