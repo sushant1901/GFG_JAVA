9ff13af26279fe2bf4413545bba08dc97f95326d
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2022-06-16 12:13:03 +0530

    nth catalan number

--- src/com/company/GFG38.java
+++ src/com/company/GFG38.java
@@ -1,53 +1,48 @@
-package com.company;
-// { Driver Code Starts
 import java.io.*;
 import java.util.*;
 import java.math.*;
-class GFG38 {
-    public static void main (String[] args) {
-
-        //taking input using Scanner class
-        Scanner sc=new Scanner(System.in);
-
-        //taking total count of testcases
-        int t=sc.nextInt();
-        while(t-->0){
-
-            //taking number of elements
-            int n=sc.nextInt();
-
-            //calling findCatalan() method
-            System.out.println(new Solution69().findCatalan(n));
-        }
-    }
+class GFG {
+	public static void main (String[] args) {
+	    
+	    //taking input using Scanner class
+		Scanner sc=new Scanner(System.in);
+		
+		//taking total count of testcases
+		int t=sc.nextInt();
+		while(t-->0){
+		    
+		    //taking number of elements
+		    int n=sc.nextInt();
+		   
+		    //calling findCatalan() method
+		    System.out.println(new Solution().findCatalan(n));
+		}
+	}
 }// } Driver Code Ends
 
 
-
-class Solution69
+class Solution
 {
-    //Function to find the nth catalan number.
-    public static BigInteger findCatalan(int n)
+    public BigInteger findCatalan(int n)
     {
-        //Your code here
-        BigInteger[]arr=new BigInteger[n+1];
-        arr[0]=BigInteger.ONE;
-        arr[1]=BigInteger.ONE;
-        for(int i=2;i<=n;i++)
-        {
-            int j=0,k=i-1;
-            BigInteger count=BigInteger.ZERO;
-            BigInteger a=BigInteger.ZERO;
-            while(j!=i)
-            {
-                count=count.add(arr[j].multiply(arr[k]));
-                j++;k--;
-            }
-            arr[i]=count;
-
+        return nthCatalan(n,new HashMap<Integer, BigInteger>());
+    }
+    public BigInteger nthCatalan(int n, HashMap<Integer, BigInteger> memo){
+        if(n <= 1){
+            return BigInteger.ONE;
         }
-        return arr[n];
-
-
+        
+        if(memo.containsKey(n)){
+            return memo.get(n);
+        }
+        
+        BigInteger num = BigInteger.ZERO;
+        
+        for(int i = 0; i < n; i++){
+            num = num.add(nthCatalan(i, memo).multiply(nthCatalan(n -i -1, memo)));
+        }
+        
+        memo.put(n, num);
+        return memo.get(n);
     }
 }
