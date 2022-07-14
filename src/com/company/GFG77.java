a6d68f57f5799ba032f16f12fc98c2644d134d29
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2022-07-14 18:50:32 +0530

    Largest suburb is zero sum

---
+++ src/com/company/GFG77.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class MaxLenZeroSumSub
+{
+
+    // Returns length of the maximum length subarray with 0 sum
+
+    // Drive method
+    public static void main(String arg[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0)
+        {
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++)
+                arr[i] = sc.nextInt();
+
+            GfG g = new GfG();
+            System.out.println(g.maxLen(arr, n));
+            T--;
+        }
+    }
+}// } Driver Code Ends
+
+
+class GfG
+{
+    int maxLen(int arr[], int n)
+    {
+        // Your code here
+        HashMap<Integer,Integer>memo=new HashMap<Integer,Integer>();
+        int prefixSum=0;
+        int ans=0;
+        memo.put(prefixSum,-1);
+        for(int i=0;i<n;i++){
+            prefixSum += arr[i];
+            if(memo.containsKey(prefixSum)){
+                ans=Math.max(ans,i-memo.get(prefixSum));
+            }else{
+                memo.put(prefixSum,i);
+            }
+            
+        }
+        return ans;
+    }
+}
