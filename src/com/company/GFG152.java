276aa59eaa9b2dc0a9eefdf8e3291354c2203170
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2022-12-22 19:05:40 +0530

    DFS in graph

---
+++ src/com/company/GFG152.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj =
+                new ArrayList<ArrayList<Integer>>();
+            for (int i = 0; i < V; i++) adj.add(new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.dfsOfGraph(V, adj);
+            for (int i = 0; i < ans.size(); i++)
+                System.out.print(ans.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+class Solution {
+    // Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfsOfGraph(int v, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        ArrayList<Integer>answer= new ArrayList<>();
+        boolean[] visited= new boolean[v];
+        
+        depthFirstSearch(adj,v,0,visited,answer);
+        return answer;
+    }
+    private void depthFirstSearch(ArrayList<ArrayList<Integer>>adj,int v, int currentVisited,boolean[] visited,ArrayList<Integer>answer){
+        if(visited[currentVisited]) return;
+        visited[currentVisited]=true;
+        
+        answer.add(currentVisited);
+        List<Integer>neighbours=adj.get(currentVisited);
+        for(int currentNeighbour:neighbours)
+            depthFirstSearch(adj,v,currentNeighbour,visited,answer);
+        
+        return;
+    }
+}
