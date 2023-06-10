153851df0d796164621de139123d173f16fbe654
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2023-06-10 11:44:27 +0530

    Arranging the array

---
+++ src/com/company/GFG222.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            obj.Rearrange(a, n);
+            
+            StringBuilder output = new StringBuilder();
+            for(int i=0;i<n;i++)
+                output.append(a[i]+" ");
+            System.out.println(output);
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution {
+    
+    public void Rearrange(int arr[], int n)
+    {
+        int j=0;
+        int temp=0;
+        for(int i=0; i<n ;i++){
+           if(arr[i]<0){
+                 int k=i;
+                temp=arr[i];
+                while(k>j){
+                    arr[k]=arr[k-1];
+                    k--;
+                }
+                arr[j]=temp;
+                j++;
+            }
+        }
+    }
+}
