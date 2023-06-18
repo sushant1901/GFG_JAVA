8b22357f322a72eeca969ab98e53b130866ebb15
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2023-06-18 15:02:59 +0530

    Ticket Counter

---
+++ src/com/company/GFG230.java
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t = Integer.parseInt(in.readLine());
+        while (t-- > 0) {
+            String s[]=in.readLine().trim().split(" ");
+            int n=Integer.parseInt(s[0]);
+            int k=Integer.parseInt(s[1]);
+            Solution ob=new Solution();
+            out.println(ob.distributeTicket(n,k));
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution {
+    public static int distributeTicket(int N,int K)
+    {
+        int quotient = N/K;
+        int remainder = N%K;
+        if(quotient%2 == 0){
+            return (quotient/2)*K+(remainder == 0 ? 1: remainder);
+        }
+        else{
+            return ((quotient/2)+1)*K+(remainder == 0 ? 0 :1);
+        }
+    }
+}
