48093c76112b23767a767c2bdff007111fb4a91b
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2023-06-21 23:04:20 +0530

    Reverse Coding

---
+++ src/com/company/GFG233.java
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S[] = read.readLine().split(" ");
+            
+            int n = Integer.parseInt(S[0]);
+
+            Solution ob = new Solution();
+            System.out.println(ob.sumOfNaturals(n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int sumOfNaturals(int n) {
+         long num=0;
+        for(int i=1;i<=n;i++){
+            num+=i;
+        }
+        int mod=(int)Math.pow(10,9)+7;
+        return(int)(num%mod);
+    }
+};
