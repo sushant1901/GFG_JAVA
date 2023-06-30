eb55444a205be77cb1cf6945da6649b545979b1f
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2023-06-30 22:18:23 +0530

    Is Binary Number Multiple of 3

---
+++ src/com/company/GFG243.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])throws IOException
+        {
+            BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+            PrintWriter out=new PrintWriter(System.out);
+            int t = Integer.parseInt(in.readLine().trim());
+            while(t-->0)
+                {
+                    String s = in.readLine().trim();
+                    Solution ob = new Solution();
+                    out.println(ob.isDivisible(s));
+                }
+                out.close();
+        }
+}    
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int isDivisible(String s) {
+        int sum = 0;
+        int x=1;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == '1'){
+                if(i%2 == 1){
+                    sum += 1; 
+                }else{
+                    sum +=2;
+                }
+            }
+        }
+        
+        return sum%3 == 0 ? 1:0;
+    }
+}
