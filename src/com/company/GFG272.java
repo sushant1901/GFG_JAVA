8f40c769d7ae715f694aac502bc89a6893f6787e
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2023-07-29 23:09:55 +0530

    Median of BST

---
+++ src/com/company/GFG272.java
@@ -0,0 +1,145 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+    	    	
+                Tree g = new Tree();
+                float answer = g.findMedian(root);
+                if(answer-(int)answer == 0)
+        		    System.out.println((int)answer);
+        		else
+        		    System.out.println(answer);
+                t--;
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Tree
+{   static int A, B;
+    public static float findMedian(Node root)
+    {
+        // code here.
+        int n=countNodes(root);
+        int i=0;
+        if(n%2!=0)
+            traverse(root,i,n/2+1,n/2+1);
+        else 
+            traverse(root,i,n/2,n/2+1);
+        return (A+B)/2f;
+    }
+    
+    static int traverse(Node root,int i,int node1,int node2){
+        if(root == null) 
+            return i;
+        i = traverse(root.left,i,node1,node2);
+        i++;
+        if(i==node1)
+            A=root.data;
+        if(i==node2)
+            B=root.data;
+        i = traverse(root.right,i,node1,node2);
+        return i;
+    }
+    
+    static int countNodes(Node root){
+        return root==null?0:1+countNodes(root.left)+countNodes(root.right);
+    }
+}
