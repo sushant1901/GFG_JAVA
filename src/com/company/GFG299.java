0c7a1a478b853d1e80fc10edf8967b99a38d89ec
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2023-09-22 23:31:23 +0530

    First and last occurrences of x

---
+++ src/com/company/GFG299.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+
+class GFG
+{
+    ArrayList<Integer> find(int arr[], int n, int x)
+    {
+        // code here
+        int s=-1,e=-1;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==x && s==-1)  s=i;
+            if(arr[arr.length-i-1]==x && e==-1)  e=arr.length-i-1;
+        }
+        ArrayList<Integer> a=new ArrayList<>();
+        a.add(s);
+        a.add(e);
+        return a;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+// Driver class
+class Array {
+
+    // Driver code
+    public static void main(String[] args) throws IOException {
+        // Taking input using buffered reader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testcases = Integer.parseInt(br.readLine());
+        // looping through all testcases
+        while (testcases-- > 0) {
+//            int n = Integer.parseInt(br.readLine());
+            String line = br.readLine();
+            String[] q = line.trim().split("\\s+");
+            int n =Integer.parseInt(q[0]);
+            int x =Integer.parseInt(q[1]);
+//            //int y =Integer.parseInt(q[2]);
+            String line1 = br.readLine();
+            String[] a1 = line1.trim().split("\\s+");
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(a1[i]);
+            }
+            GFG ob = new GFG();
+            ArrayList<Integer> ans=ob.find(arr,n,x);
+            System.out.println(ans.get(0)+" "+ans.get(1));
+        }
+    }
+}
+
+// } Driver Code Ends
