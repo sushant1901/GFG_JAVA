19fec7268fd48015c215a613529189c4d8bb81c2
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2023-11-02 11:29:17 +0530

    Minimum distance between two numbers.

---
+++ src/com/company/GFG337.java
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*; 
+class GFG{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] str = br.readLine().trim().split(" ");
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) 
+                a[i] = Integer.parseInt(str[i]);
+            String[] xy = br.readLine().trim().split(" ");
+            int x = Integer.parseInt(xy[0]);
+            int y = Integer.parseInt(xy[1]);
+            Solution g = new Solution();
+            System.out.println(g.minDist(a, n, x, y));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int minDist(int a[], int n, int x, int y) {
+        // code here
+        int xi=n+1,yi=n+1,ans=n+1;
+        for(int i=0;i<n;i++) {
+            xi=a[i]==x?i:xi;
+            yi=a[i]==y?i:yi;
+            ans=Math.min((xi!=(n+1) && yi!=(n+1))?Math.abs(xi-yi):ans,ans);
+        }
+        return ans==n+1?-1:ans;
+    }
+} 
